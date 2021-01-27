b6da03d2c4a1aeb4c34165808f7f35b20900833e
Ronan <30617930+MWRonanD@users.noreply.github.com>
2021-01-27 23:23:56 +0100

    #2 - Copy image memory

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,6 @@ int main() {
 	//Basic Usage
 	Image img("resources/images/img.jpg");
 
-	//Copy to jpg
-	img.write("../out/output-convert.png");
 	//Copy and add black line on top
 	Image copy = img;
 	//Pixel noir sur toute la largeur sur 50 pixel de haut
@@ -21,6 +19,8 @@ int main() {
 	}
 	copy.write("../out/output-copy.png");
 
+	//Copy to jpg
+	img.write("../out/output-convert.png");
 	//Blank
 	Image blank(255, 255, 3);
 	blank.write("../out/output-blank.png");
