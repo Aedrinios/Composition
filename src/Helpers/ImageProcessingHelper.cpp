d0ac1a2b798ea840d762da47f8615b158bf861f5
Aedrinios <clementclerc2@gmail.com>
2021-01-28 20:16:31 +0100

    start image processing (median)

---
+++ src/Helpers/ImageProcessingHelper.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Aedrinios on 27/01/2021.
+//
+
+#include "ImageProcessingHelper.h"
+
+Image ImageProcessingHelper::median_images(vector<Image> images) {
+    int width = images[0].getWidth();
+    int height = images[0].getHeight();
+    Image resImg(width, height,images[0].getChannel());
+
+    vector<int> red_median;
+    vector<int> green_median;
+    vector<int> blue_median;
+
+    for(int i = 0; i < width, i++){
+        for(int j = 0; j < height; j++){
+
+        }
+    }
+}
---
+++ src/Helpers/ImageProcessingHelper.h
@@ -0,0 +1,16 @@
+//
+// Created by Aedrinios on 27/01/2021.
+//
+
+#ifndef COMPOSITION_IMAGEPROCESSINGHELPER_H
+#define COMPOSITION_IMAGEPROCESSINGHELPER_H
+
+#include "Image.h"
+#include <vector>
+
+namespace ImageProcessingHelper{
+    Image median_images(vector<Image> images);
+};
+
+
+#endif //COMPOSITION_IMAGEPROCESSINGHELPER_H
